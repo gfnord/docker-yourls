9cf526d714ef64f3eb483b38d96a3569b755e67b
Gustavo Franklin Nobrega <gustavofn@gmail.com>
2016-11-30 09:41:26 -0200

    Atualizacao de configs

--- files/run.sh
+++ files/run.sh
@@ -5,5 +5,5 @@ cat /scripts/Caddyfile > /www/Caddyfile
 echo "$*" >> /www/Caddyfile
 chown web-srv:www-data /www/Caddyfile
 mkdir -p /www/public/user
-chown web-srv:www-data /www/public/user
+chown -R web-srv:www-data /www
 exec su -s /bin/sh -c "exec caddy" web-srv
